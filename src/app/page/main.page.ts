70c99eadb99f34509ffef0a705879c95eee89128
brahim <bnaitdjoudi@gmail.com>
2022-11-24 23:45:37 -0500

    prototype main page

---
+++ src/app/page/main.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-page',
+  templateUrl: 'main.page.html',
+  styleUrls: ['main.page.scss']
+})
+export class MainPage {
+
+
+  
+
+  constructor() {}
+
+}
